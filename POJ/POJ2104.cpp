27d0be5a8a0b87bb88345e7af91d79b0c17d31a6
ZJL_OIJR <34391719+ZJLOIJR@users.noreply.github.com>
2017-12-22 21:00:39 +0800

    Update POJ2104.cpp

--- POJ/POJ2104.cpp
+++ POJ/POJ2104.cpp
@@ -4,12 +4,12 @@
 #include <algorithm>
 using namespace std;
 
-const int N = 100007;
+const int N = 200007;
 
-int n, q, l, r, k, len;
+int n, q, l, r, k, len, tot = 0;
 
 int root[N];
-int a[N], b[N], rak[N];
+int a[N], b[N];
 
 inline int read()
 {
@@ -22,8 +22,7 @@ inline int read()
 
 struct SegTree
 {
-	int sum[N << 1], lson[N << 1], rson[N << 1];
-	int tot = 0;
+	int sum[N << 5], lson[N << 5], rson[N << 5];
 
 	int qrysum(int &rt, int l, int r, int ql, int qr)
 	{
@@ -59,7 +58,7 @@ struct SegTree
 
 void insert(int p, int po)
 {
-	for (; po <= n; po += po & (-po))
+	for (; po <= len; po += po & (-po))
 		ST.change(root[po], 1, n, p);
 }
 
@@ -73,27 +72,29 @@ int query(int ql, int qr, int po)
 
 void init()
 {
-	freopen("IN", "r", stdin);
+	//freopen("IN", "r", stdin);
 	n = read(), q = read();
 	for (int i = 1; i <= n; i++)
-		*(a + i) = read(), rak[i] = i;
+		*(a + i) = read();
 	memcpy(b, a, sizeof(a));
 	sort(b + 1, b + n + 1);
-	sort(rak + 1, rak + n + 1, [](int x, int y){ return a[x] < a[y]; });
 	len = unique(b + 1, b + n + 1) - b - 1;
-	for (int i = 1; i <= len; i++)
-		insert(lower_bound(a + 1, a + n + 1, b[i]) - a, i);
+	for (int i = 1; i <= n; i++)
+		insert(i, lower_bound(b + 1, b + len + 1, a[i]) - b);
 }
 
-int qrykth(int l, int r, int ql, int qr, int po)
+int qrykth(int ql, int qr, int po)
 {
-	if (l == r)
-		return l;
-	int mid = (l + r) >> 1, cnt = query(ql, qr, po - 1);
-	if (cnt >= po)
-		return qrykth(l, mid, ql, qr, po);
-	else
-		return qrykth(mid + 1, r, ql, qr, po - cnt);
+	int l = 1, r = len, ans;
+	while (l <= r)
+	{
+		int mid = (l + r) >> 1;
+		if (query(ql, qr, mid) >= po)
+			l = mid + 1, ans = mid;
+		else
+			r = mid - 1;
+	}
+	return ans;
 }
 
 void solve()
@@ -101,7 +102,7 @@ void solve()
 	while (q--)
 	{
 		l = read(), r = read(), k = read();
-		printf("%d\n", qrykth(1, n, l, r, k));
+		printf("%d\n", b[qrykth(l, r, k)]);
 	}
 }
 
