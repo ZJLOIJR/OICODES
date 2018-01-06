a08293e11c4bb75e431b26455ec080cde871678d
ZJL_OIJR <34391719+ZJLOIJR@users.noreply.github.com>
2018-01-06 11:36:55 +0800

    Update JZOJ5361.cpp

--- JZOJ/JZOJ5361.cpp
+++ JZOJ/JZOJ5361.cpp
@@ -2,13 +2,15 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
+#include <algorithm>
+using namespace std;
 
-const int N = 5e5 + 7;
+const int N = 5e5 + 7, INF = 666666;
 
 int n, a;
-int left[N], right[N];
+struct segment { int left, right; } seg[N];
 
-inline int max(int a, int b) { return a > b ? a : b; }
+inline int min(int a, int b) { return a < b ? a : b; }
 
 inline int read()
 {
@@ -21,27 +23,37 @@ inline int read()
 
 struct SegmentTree
 {
-	int mx[N];
+	int mi[N << 2];
+
+	void init(int rt, int l, int r)
+	{
+		mi[rt] = INF;
+		if (l == r)
+			return;
+		int mid = (l + r) >> 1;
+		init(rt << 1, l, mid);
+		init(rt << 1 | 1, mid + 1, r);
+	}
 
 	void insert(int rt, int l, int r, int po, int val)
 	{
 		if (l == r)
 		{
-			mx[rt] = val;
+			mi[rt] = min(mi[rt], val);
 			return;
 		}
 		int mid = (l + r) >> 1;
 		if (po <= mid) insert(rt << 1, l, mid, po, val);
 		else insert(rt << 1 | 1, mid + 1, r, po, val);
-		mx[rt] = max(mx[rt << 1], mx[rt << 1 | 1]);
+		mi[rt] = min(mi[rt << 1], mi[rt << 1 | 1]);
 	}
 
-	int qrymax(int rt, int l, int r, int ql, int qr)
+	int qrymin(int rt, int l, int r, int ql, int qr)
 	{
-		if (ql <= l && qr >= r) return mx[rt];
-		int ret = 0, mid = (l + r) >> 1;
-		if (ql <= mid) ret = max(ret, qrymax(rt << 1, l, mid, ql, qr));
-		if (mid + 1 <= qr) ret = max(ret, qrymax(rt << 1 | 1, mid + 1, r, ql, qr));
+		if (ql <= l && qr >= r) return mi[rt];
+		int ret = INF, mid = (l + r) >> 1;
+		if (ql <= mid) ret = min(ret, qrymin(rt << 1, l, mid, ql, qr));
+		if (mid + 1 <= qr) ret = min(ret, qrymin(rt << 1 | 1, mid + 1, r, ql, qr));
 		return ret;
 	}
 } Tree;
@@ -50,12 +62,29 @@ void init()
 {
 	n = read();
 	for (int i = 1; i <= n; i++)
-		a = read(), 
+		a = read(), seg[i].left = i - a < 1 ? 1 : i - a, seg[i].right = i + a > n ? n : i + a;
+	Tree.init(1, 0, n);
+	Tree.insert(1, 0, n, 0, 0);
+	sort(seg + 1, seg + n + 1, [](segment x, segment y)
+			{
+				return x.left < y.left;
+			});
+}
+
+void solve()
+{
+	for (int i = 1; i <= n; i++)
+		Tree.insert(1, 0, n, seg[i].right, Tree.qrymin(1, 0, n, seg[i].left - 1, seg[i].right) + 1);
+	printf("%d\n", Tree.qrymin(1, 0, n, n, n));
 }
 
 int main()
 {
+	freopen("cat.in", "r", stdin);
+	freopen("cat.out", "w", stdout);
 	init();
 	solve();
+	fclose(stdin);
+	fclose(stdout);
 	return 0;
 }
