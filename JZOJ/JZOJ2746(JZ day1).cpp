24cd179a2a2206d8a0dc126f8f8692ddc5e33fc8
ZJL_OIJR <34391719+ZJLOIJR@users.noreply.github.com>
2018-01-18 21:11:17 +0800

    Update JZOJ2746(JZ day1).cpp

--- JZOJ/JZOJ2746(JZ day1).cpp
+++ JZOJ/JZOJ2746(JZ day1).cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 const int N = 500007, R = 10007, C = 10007;
 
-inline int read()
+inline int read() //无耻的读入优化
 {
 	int x = 0, f = 0;
 	char c = getchar();
@@ -21,25 +21,25 @@ int left, right, mid, ans, n, r, c, a[N];
 void init()
 {
 	n = read(), r = read(), c = read();
-	for (int i = 1; i <= n; i++) *(a + i) = read();
-	sort(a + 1, a + n + 1);
+	for (int i = 1; i <= n; i++) *(a + i) = read(); //装逼读入
+	sort(a + 1, a + n + 1); //排个序，世界如此美丽
 }
 
 int check(int m)
 {
 	int cnt = 0, i = 1;
-	while (i <= n - c + 1)
+	while (i <= n - c + 1) //还没有达到序列边界
 	{
-		if (a[i + c - 1] - a[i] <= m) i = i + c, cnt++;
-		else i++;
+		if (a[i + c - 1] - a[i] <= m) i = i + c, cnt++; //能够满足条件
+		else i++; //不能
 	}
-	return cnt >= r;
+	return cnt >= r; //所选的区间个数>=r,答案可行
 }
 
 void solve()
 {
 	left = 0, right = 1e9;
-	while (left <= right)
+	while (left <= right) //二分答案
 	{
 		mid = left + right >> 1;
 		if (check(mid)) right = mid - 1, ans = mid;
