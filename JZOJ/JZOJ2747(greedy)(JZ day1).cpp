ec12a663c6f98f5f47247227a94f083115c7959f
ZJL_OIJR <34391719+ZJLOIJR@users.noreply.github.com>
2018-01-18 21:29:44 +0800

    Update JZOJ2747(greedy)(JZ day1).cpp

--- JZOJ/JZOJ2747(greedy)(JZ day1).cpp
+++ JZOJ/JZOJ2747(greedy)(JZ day1).cpp
@@ -9,10 +9,10 @@ const int M = 5e4 + 7, L = 57;
 
 int m, k, l, n, cnt = 0, tot = 0, ans = 0;
 int st[M * L / 2], to[M * L / 2], nx[M * L / 2], id[M * L / 2], pos[M * L / 2], w1[M * L / 2], w2[M * L / 2], vis[M * L / 2];
-int val[M * L / 2], son[M * L / 2][26];
+int val[M * L / 2], son[M * L / 2][26]; //玄学,必须/2,不然MLE
 char str[L];
 
-inline void add(int u, int v) { to[++tot] = v, nx[tot] = st[u], st[u] = tot; }
+inline void add(int u, int v) { to[++tot] = v, nx[tot] = st[u], st[u] = tot; } //加边
 
 void insert(char *s, int po)
 {
@@ -20,26 +20,26 @@ void insert(char *s, int po)
 	while (*s != '\0')
 	{
 		int num = *s - 'A';
-		if (!son[now][num]) son[now][num] = ++cnt, add(cnt, now);
+		if (!son[now][num]) son[now][num] = ++cnt, add(cnt, now); //字典树添加,同时建图,便于叶子节点往根走
 		now = son[now][num];
 		s++;
 	}
-	val[now]++, id[po] = now;
+	val[now]++, id[po] = now; //id[i]表示叶子节点i在字典树中的编号
 }
 
 int getw(int x)
 {
-	if (~w2[x]) return w2[x];
+	if (~w2[x]) return w2[x]; //这里使用了记忆化搜索
 	w2[x] = val[x];
-	for (int i = st[x]; i; i = nx[i]) w2[x] += getw(to[i]);
+	for (int i = st[x]; i; i = nx[i]) w2[x] += getw(to[i]); //把路上的金子数目加起来
 	return w2[x];
 }
 
 int dfs(int x)
 {
-	if (vis[x]) return 0;
-	vis[x] = 1; int ret = val[x];
-	for (int i = st[x]; i; i = nx[i]) ret += dfs(to[i]);
+	if (vis[x]) return 0; //已走过,不能走
+	vis[x] = 1; int ret = val[x]; //标记为走过
+	for (int i = st[x]; i; i = nx[i]) ret += dfs(to[i]); //把路上的金子数目加起来
 	return ret;
 }
 
@@ -55,14 +55,14 @@ void init()
 
 void solve()
 {
-	memset(w2, 0xff, sizeof(w2));
-	for (int i = 1; i <= m; i++) w1[i] = getw(id[i]);
-	for (int i = 1; i <= m; i++) pos[i] = i;
-	sort(pos + 1, pos + m + 1, [](int x, int y) { return w1[x] > w1[y]; });
+	memset(w2, 0xff, sizeof(w2)); //0xff赋进w2后是0xffffffff,即-1
+	for (int i = 1; i <= m; i++) w1[i] = getw(id[i]); //先处理出每个叶子节点所能得到的金子数
+	for (int i = 1; i <= m; i++) pos[i] = i; 
+	sort(pos + 1, pos + m + 1, [](int x, int y) { return w1[x] > w1[y]; }); //骚排序法,请查lambda表达式,仅限C++11贵族VIP7才可享用,也就是从大到小
 	memset(w1, 0, sizeof(w1));
-	for (int i = 1; i <= m; i++) w1[i] = dfs(id[pos[i]]);
-	sort(w1 + 1, w1 + m + 1);
-	for (int i = m; i >= m - n + 1; i--) ans += w1[i];
+	for (int i = 1; i <= m; i++) w1[i] = dfs(id[pos[i]]); //从叶子节点往根走,统计出金子数
+	sort(w1 + 1, w1 + m + 1); //从小到大排序
+	for (int i = m; i >= m - n + 1; i--) ans += w1[i]; //从大到小选n个
 	printf("%d\n", ans);
 }
 
