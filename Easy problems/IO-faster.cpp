431d4010f82067a45faf547351166d10bac151da
ZJL_OIJR <34391719+ZJLOIJR@users.noreply.github.com>
2018-03-03 01:19:42 +0000

    Update
    Easier

--- Easy problems/IO-faster.cpp
+++ Easy problems/IO-faster.cpp
@@ -3,6 +3,6 @@ inline int read()
 	int x = 0, f = 0;
 	char c = getchar();
 	for (; c < '0' || c > '9'; c = getchar()) if (c == '-') f = 1;
-	for (; c >= '0' && c <= '9'; c = getchar()) x = (x << 1) + (x << 3) + c - '0';
+	for (; c >= '0' && c <= '9'; c = getchar()) x = (x << 1) + (x << 3) + (c ^ '0');
 	return f ? -x : x;
 }
