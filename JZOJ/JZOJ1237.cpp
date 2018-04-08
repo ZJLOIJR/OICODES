4175fea34bf72f4d2ed2630cb6e65d629771569f
ZJL_OIJR <34391719+ZJLOIJR@users.noreply.github.com>
2018-04-08 11:19:03 +0000

    Update JZOJ1237.cpp

--- JZOJ/JZOJ1237.cpp
+++ JZOJ/JZOJ1237.cpp
@@ -6,9 +6,10 @@
 const int N = 2e3 + 3;
 
 int n, m, ans;
-int f[N][N];
 char map[N][N];
+int f[N][N];
 
+inline int min(int a, int b) { return a < b ? a : b; }
 inline int max(int a, int b) { return a > b ? a : b; }
 
 int main()
@@ -16,15 +17,26 @@ int main()
 	scanf("%d%d", &n, &m);
 	for (int i = 1; i <= n; i++)
 		for (int j = 1; j <= m; j++)
+		{
 			scanf(" %c", &map[i][j]);
+			if (map[i][j] == '.')
+			{
+				f[i][j] = 1;
+				if (map[i][j - 1] == '.') f[i][j] += f[i][j - 1];
+			}
+		}
 	ans = 0;
 	for (int i = 1; i <= n; i++)
 		for (int j = 1; j <= m; j++)
-		{
-			if (map[i][j] == 'X') continue;
-			if (map[i - 1][j] == '.') f[i][j] = max(f[i][j], f[i - 1][j] + 2);
-			if (map[i][j - 1] == '.') f[i][j] = max(f[i][j], f[i][j - 1] + 2);
-		}
-	printf("%d\n", ans);
+			if (map[i][j] == '.')
+			{
+				int minn = 2147483647;
+				for (int k = i; k >= 1 && map[k][j] == '.'; k--)
+				{
+					minn = min(minn, f[k][j]);
+					ans = max(ans, ((i - k + 1) + minn) * 2);
+				}
+			}
+	printf("%d\n", ans - 1);
 	return 0;
 }
