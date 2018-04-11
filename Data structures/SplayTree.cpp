12e596854a50de72c4c6517d8513306f569b48c2
ZJL_OIJR <34391719+ZJLOIJR@users.noreply.github.com>
2018-04-11 09:38:21 +0000

    Update SplayTree.cpp

--- Data structures/SplayTree.cpp
+++ Data structures/SplayTree.cpp
@@ -19,7 +19,6 @@ struct SplayTree
 		fa[x] = z, son[z][get(y)] = x;
 		son[x][!kind] = y, fa[y] = x;
 	}
-
 	void splay(int x, int anc)
 	{
 		while (fa[x] != anc)
@@ -30,31 +29,26 @@ struct SplayTree
 		}
 		if (anc == 0) root = x;
 	}
-
 	int search(int x)
 	{
 		int now = root;
 		while (1)
 		{
-			if (!now) break;
-			if (key[now] == x)
-			{
-				splay(now, 0);
-				return now;
-			}
+			if (!now) return 0;
+			if (key[now] == x) return now;
 			now = son[now][x > key[now]];
 		}
 		return 0;
 	}
-
+	
 	void insert(int x)
 	{
 		if (!root)
 			root = ++tot, fa[tot] = 0, son[tot][0] = son[tot][1] = 0, cnt[tot] = 1, siz[tot] = 1, key[tot] = x;
 		else
 		{
-			int id = search(x);
-			if (id) { cnt[id]++; return; }
+			int tmp = search(x);
+			if (tmp) { cnt[tmp]++; return; }
 			int now = root;
 			while (1)
 			{
@@ -62,7 +56,7 @@ struct SplayTree
 				if (!son[now][x > key[now]])
 				{
 					son[now][x > key[now]] = ++tot;
-					fa[tot] = now, son[tot][0] = son[tot][1] = 0, cnt[tot] = 1, ;
+					fa[tot] = now, son[tot][0] = son[tot][1] = 0, cnt[tot] = 1, siz[tot] = 1, key[tot] = x;
 					splay(tot, 0);
 					break;
 				}
@@ -71,14 +65,64 @@ struct SplayTree
 		}
 	}
 
+	void erase(int x)
+	{
+		int now = search(x);
+		if (cnt[now] > 1) cnt[now]--;
+		else
+		{
+			splay(now, 0);
+			if (!son[now][0])
+			{
+				root = son[now][1];
+				fa[root] = 0;
+			}
+			else
+			{
+				int tmp = son[now][0];
+				while (son[tmp][1]) tmp = son[tmp][1];
+				splay(tmp, now);
+				root = tmp;
+				son[tmp][1] = son[now][1], fa[son[now][1]] = tmp;
+			}
+		}
+	}
+
 	int getpre(int x)
 	{
 		insert(x);
-		splay(search(x), 0);
-
-		
+		int now = search(x);
+		splay(now, 0);
+		int tmp = son[tmp][0];
+		while (son[tmp][1]) tmp = son[tmp][1];
+		erase(x);
+		return tmp;
 	}
-}
+	
+	int getnex(int x)
+	{
+		insert(x);
+		int now = search(x);
+		splay(now, 0);
+		int tmp = son[tmp][1];
+		while (son[tmp][0]) tmp = son[tmp][0];
+		erase(x);
+		return tmp;
+	}
+
+	int getrank(int x)
+	{
+		insert(x);
+		int now = search(x);
+		erase(x);
+		return siz[son[now][0]] + 1;
+	}
+
+	int getkth(int k)
+	{
+
+	}
+} tree;
 
 int main()
 {
