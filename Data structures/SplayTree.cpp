925d6b06037954340d035934dfdab9e2466b9809
ZJL_OIJR <34391719+ZJLOIJR@users.noreply.github.com>
2018-04-12 05:33:15 +0000

    Update SplayTree.cpp

--- Data structures/SplayTree.cpp
+++ Data structures/SplayTree.cpp
@@ -3,163 +3,107 @@
 #include <cstring>
 #include <cstdlib>
 
-typedef long long ll;
-const int N = 1e5 + 3;
+const int N = 3e5 + 3;
 
-int root = 0, tot = 0;
-int q, opt, x;
+int root, tot = 0, q, opt, x;
 struct SplayTree
 {
-	int fa[N], son[N][2], cnt[N], siz[N], key[N];
-
+	int key[N], son[N][2], fa[N], siz[N], cnt[N];
+	void upd(int x) { siz[x] = siz[son[x][0]] + siz[son[x][1]] + cnt[x]; }
 	int get(int x) { return son[fa[x]][1] == x; }
-	void upd(int x) { if (x) siz[x] = siz[son[x][0]] + siz[son[x][1]] + 1; }
 	void rotate(int x)
 	{
-		int y = fa[x], z = fa[y], kind = get(x);
-		son[y][kind] = son[x][!kind], fa[son[x][!kind]] = y;
-		fa[x] = z, son[z][get(y)] = x;
-		son[x][!kind] = y, fa[y] = x;
-		upd(y), upd(x);
+		int y = fa[x], z = fa[y], k = get(x);
+		son[y][k] = son[x][!k]; if (son[x][!k]) fa[son[x][!k]] = y;
+		son[x][!k] = y, fa[y] = x;
+		fa[x] = z; if (z) son[z][son[z][1] == y] = x;
+		upd(y);
 	}
 	void splay(int x, int anc)
 	{
-		while (fa[x] != anc)
+		for (int y, z; (y = fa[x]) != anc; rotate(x))
 		{
-			int y = fa[x];
-			if (fa[y] == anc) rotate(x);
-			else rotate(get(x) == get(y) ? y : x);
-			printf("splay\n");
+			z = fa[y];
+			if (z == anc) continue;
+			rotate(get(x) == get(y) ? y : x);
 		}
-		if (anc == 0) root = x;
+		if (!anc) root = x, upd(x);
 	}
-	int search(int x)
-	{
-		int now = root;
-		while (1)
-		{
-			if (!now) return 0;
-			if (key[now] == x) return now;
-			now = son[now][x > key[now]];
-			printf("search\n");
-		}
-		return 0;
-	}
-	
 	void insert(int x)
 	{
-		if (!root)
-			root = ++tot, fa[tot] = 0, son[tot][0] = son[tot][1] = 0, cnt[tot] = 1, siz[tot] = 1, key[tot] = x;
+		int y = 0, now = root;
+		while (now && key[now] != x) { y = now; now = son[now][x > key[now]]; }
+		if (now) cnt[now]++;
 		else
 		{
-			int tmp = search(x);
-			if (tmp) { cnt[tmp]++; return; }
-			int now = root;
-			while (1)
-			{
-				siz[now]++;
-				if (!son[now][x > key[now]])
-				{
-					son[now][x > key[now]] = ++tot;
-					fa[tot] = now, son[tot][0] = son[tot][1] = 0, cnt[tot] = 1, siz[tot] = 1, key[tot] = x;
-					splay(tot, 0);
-					break;
-				}
-				now = son[now][x > key[now]];
-				printf("insert\n");
-			}
+			now = ++tot;
+			siz[now] = cnt[now] = 1, key[now] = x, fa[now] = y;
+			if (y) son[y][x > key[y]] = now;
 		}
-	}
-
-	void erase(int x)
-	{
-		int now = search(x);
-		if (cnt[now] > 1) cnt[now]--;
-		else
-		{
-			splay(now, 0);
-			if (!son[now][0])
-			{
-				root = son[now][1];
-				fa[root] = 0;
-				upd(root);
-			}
-			else
-			{
-				int tmp = son[now][0];
-				while (son[tmp][1]) tmp = son[tmp][1];
-				splay(tmp, now);
-				root = tmp;
-				son[tmp][1] = son[now][1], fa[son[now][1]] = tmp;
-				upd(root);
-			}
-		}
-	}
-
-	int getpre(int x)
-	{
-		insert(x);
-		int now = search(x);
 		splay(now, 0);
-		int tmp = son[now][0];
-		while (son[tmp][1]) tmp = son[tmp][1];
-		erase(x);
-		return tmp;
 	}
-	
-	int getnex(int x)
+	void find(int x)
 	{
-		insert(x);
-		int now = search(x);
+		if (!root) return;
+		int now = root;
+		while (son[now][x > key[now]] && key[now] != x) now = son[now][x > key[now]];
 		splay(now, 0);
-		int tmp = son[now][1];
-		while (son[tmp][0]) tmp = son[tmp][0];
-		erase(x);
-		return tmp;
 	}
-
 	int getrank(int x)
 	{
-		insert(x);
-		int now = root, ret = 0;
-		while (1)
-		{
-			if (x == key[now]) { ret += siz[son[now][0]] + 1; break; }
-			if (x > key[now]) ret += siz[son[now][0]] + 1;
-			now = son[now][x > key[now]];
-		}
-		erase(x);
-		return ret;
+		find(x);
+		return siz[son[root][0]];
 	}
-
 	int getkth(int k)
 	{
-		int now = root, cnt;
+		if (k > siz[root]) return 0;
+		int now = root, tmp;
 		while (1)
 		{
-			cnt = siz[son[now][0]];
-			if (cnt == k - 1) break;
-			else if (cnt < k - 1) now = son[now][1], k -= cnt + 1;
-			else now = son[now][0];
+			tmp = son[now][0];
+			if (siz[tmp] + cnt[now] < k)
+			{
+				k -= siz[tmp] + cnt[now];
+				now = son[now][1];
+			}
+			else if (siz[tmp] >= k) now = tmp;
+			else return key[now];
 		}
+	}
+	int getnex(int x, int k)
+	{
+		find(x);
+		if (((key[root] > x) && k) || ((key[root] < x) && !k)) return root;
+		int now = son[root][k];
+		while (son[now][!k]) now = son[now][!k];
 		return now;
+	}
+	void del(int x)
+	{
+		int pre = getnex(x, 0), nex = getnex(x, 1);
+		splay(pre, 0), splay(nex, pre);
+		int tmp = son[nex][0];
+		if (cnt[tmp] > 1) cnt[tmp]--, splay(tmp, 0);
+		else son[nex][0] = 0;
 	}
 } tree;
 
 int main()
 {
 	scanf("%d", &q);
+	tree.insert(-0x7f7f7f7f), tree.insert(0x7f7f7f7f);
 	while (q--)
 	{
 		scanf("%d%d", &opt, &x);
 		if (opt == 1) tree.insert(x);
-		else if (opt == 2) tree.erase(x);
+		else if (opt == 2) tree.del(x);
 		else if (opt == 3) printf("%d\n", tree.getrank(x));
-		else if (opt == 4) printf("%d\n", tree.key[tree.getkth(x)]);
-		else if (opt == 5) printf("%d\n", tree.key[tree.getpre(x)]);
-		else if (opt == 6) printf("%d\n", tree.key[tree.getnex(x)]);
+		else if (opt == 4) printf("%d\n", tree.getkth(x + 1));
+		else if (opt == 5) printf("%d\n", tree.key[tree.getnex(x, 0)]);
+		else if (opt == 6) printf("%d\n", tree.key[tree.getnex(x, 1)]);
 	}
 	return 0;
 }
+
 //5632
 //https://blog.csdn.net/gengmingrui/article/details/49978543
