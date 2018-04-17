6910c342aa69a42b80d8e402bcb3e0773c2b9cdb
ZJL_OIJR <34391719+ZJLOIJR@users.noreply.github.com>
2018-04-17 05:29:43 +0000

    Update BZOJ3223.cpp

--- BZOJ/BZOJ3223.cpp
+++ BZOJ/BZOJ3223.cpp
@@ -2,141 +2,90 @@
 #include <cstdio>
 #include <cstring>
 #include <cstdlib>
-#include <algorithm>
-using namespace std;
- 
-const int N = 100007;
- 
-int n, q, u, v;
- 
-inline int read()
-{
-    int x = 0, f = 0;
-    char c = getchar();
-    for (; c < '0' || c > '9'; c = getchar()) if (c == '-') f = 1;
-    for (; c >= '0' && c <= '9'; c = getchar()) x = (x << 1) + (x << 3) + c - '0';
-    return f ? -x : x;
-}
- 
-inline void swap(int &a, int &b)
-{
-    int t = a;
-    a = b, b = t;
-}
- 
+
+const int N = 1e5 + 3, INF = 0x3f3f3f3f;
+
+void swap(int &a, int &b) { int t = a; a = b, b = t; }
+
+int root, tot = 0, n, q, l, r;
 struct SplayTree
 {
-    int fa[N], son[N][2], key[N], tag[N], siz[N];
-    int root, tot;
- 
-    void init() { tot = 0, root = 0; }
- 
-    void down(int x)
-    {
-        if (x == 0 || tag[x] == 0) return;
-        swap(son[x][0], son[x][1]);
-        tag[son[x][0]] ^= 1, tag[son[x][1]] ^= 1;
-        tag[x] = 0;
-    }
- 
-    void update(int x) { if (x) siz[x] = siz[son[x][0]] + siz[son[x][1]] + 1; }
- 
-    int get(int x) { return son[fa[x]][1] == x; }
- 
-    void rotate(int x)
-    {
-        int y = fa[x], z = fa[y], kind = !get(x);
-        son[y][!kind] = son[x][kind];
-        if (son[x][kind])
-            fa[son[x][kind]] = y;
-        fa[x] = z;
-        if (z)
-            son[z][get(y)] = x;
-        son[x][kind] = y, fa[y] = x;
-        down(y), down(x);
-        update(y), update(x);
-    }
- 
-    void splay(int x, int anc)
-    {
-        while (fa[x] != anc)
-        {
-            int y = fa[x];
-            if (fa[y] == anc)
-                rotate(x);
-            else
-                rotate(get(x) == get(y) ? y : x);
-        }
-        if (anc == 0)
-            root = x;
-    }
- 
-    void insert(int x)
-    {
-        if (!root)
-            root = ++tot, fa[root] = 0, key[root] = x, tag[root] = 0, siz[root] = 1;
-        else
-        {
-            int now = root;
-            while (2333)
-            {
-                siz[now]++;
-                if (!son[now][x > key[now]])
-                {
-                    son[now][x > key[now]] = ++tot, fa[tot] = now, key[tot] = x, tag[tot] = 0, siz[tot] = 1;
-                    break;
-                }
-                now = son[now][x > key[now]];
-            }
-            splay(tot, 0);
-        }
-    }
- 
-    int findkth(int k)
-    {
-        int now = root, cnt;
-        while (2333)
-        {
-            down(now);
-            cnt = siz[son[now][0]];
-            if (k - 1 == cnt) return now;
-            else if (k - 1 < cnt) now = son[now][0];
-            else now = son[now][1], k -= cnt + 1;
-        }
-    }
- 
-    void reverse(int l, int r)
-    {
-        int lx = findkth(l), rx = findkth(r + 2);
-        splay(lx, 0);
-        splay(rx, lx);
-        tag[son[son[root][1]][0]] ^= 1;
-    }
- 
-    void print(int x)
-    {
-        if (x == 0)
-            return;
-        down(x);
-        print(son[x][0]);
-        if (key[x] != 2147483647 && key[x] != -2147483647) printf("%d ", key[x]);
-        print(son[x][1]);
-    }
+	int fa[N], son[N][2], rev[N], key[N], siz[N];
+
+	void down(int x)
+	{
+		if (!rev[x] || !x) return;
+		swap(son[x][0], son[x][1]);
+		rev[x] = 0, rev[son[x][0]] ^= 1, rev[son[x][1]] ^= 1;
+	}
+	void upd(int x) { siz[x] = siz[son[x][0]] + siz[son[x][1]] + 1; }
+	void rotate(int x)
+	{
+		int y = fa[x], z = fa[y], k = (son[y][1] == x);
+		son[y][k] = son[x][!k];
+		if (son[x][!k]) fa[son[x][!k]] = y;
+		son[x][!k] = y, fa[y] = x, fa[x] = z;
+		if (z) son[z][son[z][1] == y] = x;
+		down(y), down(x), upd(y), upd(x);
+	}
+	void splay(int x, int anc)
+	{
+		for (int y, z; (y = fa[x]) != anc; rotate(x))
+		{
+			z = fa[y];
+			if (z == anc) continue;
+			rotate((son[y][0] == x) == (son[z][0] == y) ? y : x);
+		}
+		if (!anc) root = x, upd(x), down(x);
+	}
+	void insert(int x)
+	{
+		int now = root, y = 0;
+		while (now && key[now] != x) { y = now, now = son[now][x > key[now]]; }
+		now = ++tot;
+		fa[now] = y, rev[now] = 0, key[now] = x, siz[now] = 1;
+		if (y) son[y][x > key[y]] = now;
+		splay(now, 0);
+	}
+	int getkth(int k)
+	{
+		if (k > siz[root]) return 0;
+		int now = root, tmp;
+		while (1)
+		{
+			tmp = son[now][0];
+			if (siz[tmp] + 1 < k) k -= siz[tmp] + 1, now = son[now][1];
+			else if (siz[tmp] >= k) now = tmp;
+			else return now;
+		}
+	}
+	void reverse(int l, int r)
+	{
+		int lx = getkth(l), rx = getkth(r + 2);
+		splay(lx, 0), splay(rx, lx);
+		rev[son[rx][0]] ^= 1;
+	}
+	void print(int x)
+	{
+		if (!x) return;
+		down(x);
+		print(son[x][0]);
+		if (key[x] != INF && key[x] != -INF) printf("%d ", key[x]);
+		print(son[x][1]);
+	}
 } tree;
- 
+
 int main()
 {
-    tree.init();
-    n = read(), q = read();
-    tree.insert(-2147483647);
-    for (int i = 1; i <= n; i++)
-        tree.insert(i);
-    tree.insert(2147483647);
-    while (q--)
-    {
-        u = read(), v = read();
-        tree.reverse(u, v);
-    }
-    tree.print(tree.root);
-    return 0;
+	scanf("%d%d", &n, &q);
+	tree.insert(INF);
+	for (int i = 1; i <= n; i++) tree.insert(i);
+	tree.insert(-INF);
+	while (q--)
+	{
+		scanf("%d%d", &l, &r);
+		tree.reverse(l, r);
+	}
+	tree.print(root);
+	return 0;
 }
