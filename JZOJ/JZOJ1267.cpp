7da0c5efac50aae25c13a1e403074313107929c5
ZJL_OIJR <34391719+ZJLOIJR@users.noreply.github.com>
2018-04-24 04:47:56 +0000

    Update JZOJ1267.cpp

--- JZOJ/JZOJ1267.cpp
+++ JZOJ/JZOJ1267.cpp
@@ -15,7 +15,7 @@ inline int read()
 }
 
 int n, m, u, v, w, head, tail, tot = 0;
-int st[N], to[M << 1], nx[M << 1], len[M << 1], dis[N][2], que[N * 20], vis[N];
+int st[N], to[M << 1], nx[M << 1], len[M << 1], dis[N][2], que[N * 20][2], vis[N];
 
 inline void add(int u, int v, int w) { to[++tot] = v, nx[tot] = st[u], len[tot] = w, st[u] = tot; }
 
@@ -29,20 +29,25 @@ int main()
 	n = read(), m = read();
 	for (int i = 1; i <= m; i++) u = read(), v = read(), w = read(), add(u, v, w), add(v, u, w);
 	head = 1, tail = 0;
-	que[++tail] = 1, vis[1] = 1, dis[1][0] = 0;
+	que[++tail][0] = 1, que[tail][1] = 0, vis[1] = 1, dis[1][0] = 0;
 	while (head <= tail)
 	{
-		int u = que[head++];
-		vis[u] = 0;
+		int u = que[head][0], d = que[head][1];
+		head++, vis[u] = 0;
 		for (int i = st[u]; i; i = nx[i])
 		{
-			int v = to[i];
-			if (dis[u][0] + len[i] < dis[v][0])
+			int v = to[i], w = d + len[i];
+			if (w < dis[v][0])
 			{
-				dis[v][0] = dis[u][0] + len[i];
-				if (!vis[v]) que[++tail] = v, vis[v] = 1;
+				int tmp = w;
+				w = dis[v][0], dis[v][0] = tmp;
+				que[++tail][0] = v, que[tail][1] = dis[v][0], vis[v] = 1;
 			}
-			if (dis[u][0] + len[i] > dis[v][0] && dis[u][0] + len[i] < dis[v][1]) dis[v][1] = dis[u][0] + len[i];
+			if (w > dis[v][0] && w < dis[v][1])
+			{
+				dis[v][1] = w;
+				que[++tail][0] = v, que[tail][1] = dis[v][1], vis[v] = 1;
+			}
 		}
 	}
 	printf("%d\n", dis[n][1]);
