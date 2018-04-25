5b151839a41f7cb878dd9c138b7b2a5655e81764
ZJL_OIJR <34391719+ZJLOIJR@users.noreply.github.com>
2018-04-25 09:36:57 +0000

    Update JZOJ5661.cpp

--- JZOJ/JZOJ5661.cpp
+++ JZOJ/JZOJ5661.cpp
@@ -16,43 +16,40 @@ inline int read()
 }
 
 int n, p, f, tot = 0, cnt = 0, ans;
-int st[N], to[N], nx[N], siz[N], dfn[N], id[N], w[N], v[N];
+int st[N], to[N << 1], nx[N << 1], siz[N], dfn[N], w[N], v[N];
 int dp[N][P];
 
 inline void add(int u, int v) { to[++tot] = v, nx[tot] = st[u], st[u] = tot; }
 
 void dfs(int u, int from)
 {
-	dfn[id[u] = ++cnt] = u;
+	siz[u] = 1, dfn[++cnt] = u;
 	for (int i = st[u]; i; i = nx[i])
-	{
-		int v = to[i];
-		if (v != from) dfs(v, u), siz[v] += siz[u];
-	}
+		if (to[i] != from) dfs(to[i], u), siz[u] += siz[to[i]];
 }
 
 int main()
 {
-	//freopen("medicine.in", "r", stdin);
-	//freopen("medicine.out", "w", stdout);
+	freopen("medicine.in", "r", stdin);
+	freopen("medicine.out", "w", stdout);
 
 	n = read(), p = read();
 	for (int i = 1; i <= n; i++)
 	{
 		w[i] = read(), f = read(), v[i] = read();
-		if (i != f) add(f, i);
+		if (i != f) add(f, i), add(i, f);
 	}
 	dfs(1, 0);
 	memset(dp, -0x3f, sizeof(dp));
 	dp[1][0] = ans = 0;
 	for (int i = 1; i <= cnt; i++)
 	{
-		int x = id[i];
+		int x = dfn[i];
 		for (int j = 0; j <= p; j++)
 		{
 			if (dp[i][j] < 0) continue;
 			if (j + w[x] <= p) dp[i + 1][j + w[x]] = max(dp[i + 1][j + w[x]], dp[i][j] + v[x]);
-			dp[x + siz[x]][j] = max(dp[x + siz[x]][j], dp[i][j]);
+			dp[i + siz[x]][j] = max(dp[i + siz[x]][j], dp[i][j]);
 			ans = max(ans, dp[i][j]);
 		}
 	}
