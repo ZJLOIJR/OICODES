b405a86bdae663f7eed3b3558f92887bc4296365
ZJL_OIJR <34391719+ZJLOIJR@users.noreply.github.com>
2018-04-25 05:34:12 +0000

    Create JZOJ5661.cpp

---
+++ JZOJ/JZOJ5661.cpp
@@ -0,0 +1,65 @@
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#define max(a, b) ((a) > (b) ? (a) : (b))
+
+const int N = 5e3 + 3, P = 1e4 + 3;
+
+inline int read()
+{
+	int x = 0, f = 0;
+	char c = getchar();
+	for (; c < '0' || c > '9'; c = getchar()) if (c == '-') f = 1;
+	for (; c >= '0' && c <= '9'; c = getchar()) x = (x << 1) + (x << 3) + (c ^ '0');
+	return f ? -x : x;
+}
+
+int n, p, f, tot = 0, cnt = 0, ans;
+int st[N], to[N], nx[N], siz[N], dfn[N], id[N], w[N], v[N];
+int dp[N][P];
+
+inline void add(int u, int v) { to[++tot] = v, nx[tot] = st[u], st[u] = tot; }
+
+void dfs(int u, int from)
+{
+	dfn[id[u] = ++cnt] = u;
+	for (int i = st[u]; i; i = nx[i])
+	{
+		int v = to[i];
+		if (v != from) dfs(v, u), siz[v] += siz[u];
+	}
+}
+
+int main()
+{
+	//freopen("medicine.in", "r", stdin);
+	//freopen("medicine.out", "w", stdout);
+
+	n = read(), p = read();
+	for (int i = 1; i <= n; i++)
+	{
+		w[i] = read(), f = read(), v[i] = read();
+		if (i != f) add(f, i);
+	}
+	dfs(1, 0);
+	memset(dp, -0x3f, sizeof(dp));
+	dp[1][0] = ans = 0;
+	for (int i = 1; i <= cnt; i++)
+	{
+		int x = id[i];
+		for (int j = 0; j <= p; j++)
+		{
+			if (dp[i][j] < 0) continue;
+			if (j + w[x] <= p) dp[i + 1][j + w[x]] = max(dp[i + 1][j + w[x]], dp[i][j] + v[x]);
+			dp[x + siz[x]][j] = max(dp[x + siz[x]][j], dp[i][j]);
+			ans = max(ans, dp[i][j]);
+		}
+	}
+	for (int i = 0; i <= p; i++) ans = max(ans, dp[cnt + 1][i]);
+	printf("%d\n", ans);
+
+	fclose(stdin);
+	fclose(stdout);
+	return 0;
+}
