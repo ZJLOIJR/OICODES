56e62d273f41d6ace0c0e4027fdd3cc0dafb16ce
ZJL_OIJR <34391719+ZJLOIJR@users.noreply.github.com>
2018-05-03 04:48:50 +0000

    Update JZOJ2972.cpp

--- JZOJ/JZOJ2972.cpp
+++ JZOJ/JZOJ2972.cpp
@@ -5,16 +5,18 @@
 
 const int N = 5e4 + 3;
 
-int n, m, tot = 0, ans = 0, x;
-int fa[N], leaf[N];
+int n, m, ans, tot;
+int fa[N], leaf[N], sons[N];
 
 int main()
 {
+	sons[tot = 1] = 0;
 	scanf("%d%d", &n, &m);
+	int las = 1;
 	while (tot < n)
 	{
-		fa[++tot] = x, leaf[x = tot] = 0;
-		for (int i = 1; i <= m - 1 && tot < n; i++) leaf[++tot] = 1, fa[tot] = x;
+		if (sons[las] < m) fa[++tot] = las, sons[las]++, leaf[tot] = 1;
+		else leaf[++las] = 0;
 	}
 	for (int i = 1; i <= n; i++) if (leaf[i]) ans++;
 	printf("%d\n", ans);
