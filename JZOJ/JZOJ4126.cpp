7fafce579ef12f6b44a1fd64c776ce8e642a959c
ZJL_OIJR <34391719+ZJLOIJR@users.noreply.github.com>
2018-05-29 05:32:54 +0000

    Update JZOJ4126.cpp

--- JZOJ/JZOJ4126.cpp
+++ JZOJ/JZOJ4126.cpp
@@ -4,55 +4,51 @@
 #include <cstdlib>
 #include <algorithm>
 using namespace std;
-
+ 
+typedef unsigned long long ull;
 typedef long long ll;
 const int LEN = 4e6 + 7;
-const ll P = 1e7 + 9;
-
-int tmp[P], buc[P];
+const int P = 9191891;
+ 
+int tmp[P];
 int s, t, n, m, cnt = 0, len = 0, goal, arrlen = 0;
 char st[LEN], str[LEN];
-ll ans = 0, sum[LEN], pow[LEN], arr[LEN];
-
-inline ll hashval(int l, int r) { return (sum[r] - sum[l - 1] * pow[r - l + 1] % P + P) % P; }
-inline int insert(int hasharr[], ll x)
-{
-	for (int i = x % P; hasharr[i] != x && hasharr[i] != -1; ) i = (i == P) ? 0 : i + 1;
-	hasharr[i]++;
-	return i;
-}
-
+ull sum[LEN], pow[LEN], arr[LEN];
+ll ans = 0;
+ 
+inline ull hashval(int l, int r) { return sum[r] - sum[l - 1] * pow[r - l + 1]; }
+ 
 int main()
 {
-	pow[0] = 1; for (int i = 1; i <= LEN - 7; i++) pow[i] = pow[i - 1] * 29 % P;
-	scanf("%d%d%d%d", &s, &t, &n, &m); getchar();
-	for (char ch = getchar(); ch != EOF; ch = getchar()) if (ch >= 'a' && ch <= 'z') st[++cnt] = ch;
-	if (n < m)
-	{
-		for (int i = s * n + 1; i <= cnt; i++) str[++len] = st[i];
-		for (int i = 1; i <= s * n; i++) str[++len] = st[i];
-		n ^= m ^= n ^= m;
-		s ^= t ^= s ^= t;
-	}
-	else for (int i = 1; i <= cnt; i++) str[++len] = st[i];
-	for (int i = 1; i <= len; i++) sum[i] = (sum[i - 1] * 29 % P + str[i] - 'a') % P;
-	memset(buc, -1, sizeof(buc));
-	for (int i = s * n + 1; i <= len; i += m) insert(buc, hashval(i, i + m - 1));
-	sort(arr + 1, arr + arrlen + 1);
-	goal = n + m >> 1;
-	for (int i = 1; i <= s * n; i += n)
-	{
-		memset(tmp, 0, sizeof(tmp));
-		ull goalval = hashval(i + goal, i + n - 1);
-		for (int j = i; j <= i + m; j++)
-			if (hashval(j, j + (n - goal) - 1) == goalval)
-			{
-				int l = j - 1, r = j + (n - goal);
-				ull ret = (hashval(r, i + goal - 1) * pow[l - i + 1] % P + hashval(i, l)) % P;
-				if (!tmp[ret]) ans += buc[insert()]
-				tmp[ret] = 1;
-			}
-	}
-	printf("%lld\n", ans);
-	return 0;
+    pow[0] = 1; for (int i = 1; i <= LEN - 7; i++) pow[i] = pow[i - 1] * 29;
+    scanf("%d%d%d%d", &s, &t, &n, &m); getchar();
+    for (char ch = getchar(); ch != EOF; ch = getchar()) if (ch >= 'a' && ch <= 'z') st[++cnt] = ch;
+    if (n < m)
+    {
+        for (int i = s * n + 1; i <= cnt; i++) str[++len] = st[i];
+        for (int i = 1; i <= s * n; i++) str[++len] = st[i];
+        n ^= m ^= n ^= m;
+        s ^= t ^= s ^= t;
+    }
+    else for (int i = 1; i <= cnt; i++) str[++len] = st[i];
+    for (int i = 1; i <= len; i++) sum[i] = sum[i - 1] * 29 + str[i] - 'a';
+    for (int i = s * n + 1; i <= len; i += m) arr[++arrlen] = hashval(i, i + m - 1);
+    sort(arr + 1, arr + arrlen + 1);
+    goal = n + m >> 1;
+    for (int i = 1; i <= s * n; i += n)
+    {
+        ull goalval = hashval(i + goal, i + n - 1);
+        for (int j = i; j <= i + m; j++)
+            if (hashval(j, j + (n - goal) - 1) == goalval)
+            {
+                int l = j - 1, r = j + (n - goal);
+                ull ret = hashval(r, i + goal - 1) * pow[l - i + 1] + hashval(i, l);
+                int ls = lower_bound(arr + 1, arr + arrlen + 1, ret) - arr, rs = upper_bound(arr + 1, arr + arrlen + 1, ret) - arr - 1;
+                if (arr[ls] != ret) continue;
+                if (tmp[ret % P] != i) ans += rs - ls + 1;
+                tmp[ret % P] = i;
+            }
+    }
+    printf("%lld\n", ans);
+    return 0;
 }
