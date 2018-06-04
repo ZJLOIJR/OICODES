61c696304684cecd30a38873e1055a57f3b6f886
ZJL_OIJR <34391719+ZJLOIJR@users.noreply.github.com>
2018-06-04 05:18:49 +0000

    Update JZOJ3535.cpp

--- JZOJ/JZOJ3535.cpp
+++ JZOJ/JZOJ3535.cpp
@@ -1,17 +1,25 @@
 #include <cstdio>
+#include <cstring>
+#include <cstdlib>
 
-int n, las = 0, x;
+const int N = 1e5 + 7;
+
+int n, a[N];
 long long ans = 0;
+
+long long solve(int l, int r)
+{
+	if (l > r) return 0;
+	int mi = 0x3f3f3f3f, po = 0;
+	for (int i = l; i <= r; i++) if (a[i] < mi) mi = a[i], po = i;
+	for (int i = l; i <= r; i++) a[i] -= mi;
+	return mi + solve(l, po - 1) + solve(po + 1, r);
+}
 
 int main()
 {
 	scanf("%d", &n);
-	while (n--)
-	{
-		scanf("%d", &x);
-		if (x > las) ans += x - las;
-		las = x;
-	}
-	printf("%lld\n", ans);
+	for (int i = 1; i <= n; i++) scanf("%d", a + i);
+	printf("%lld\n", solve(1, n));
 	return 0;
 }
