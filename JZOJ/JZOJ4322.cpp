d1227d0bfbe6eba90adc5d143e5f15c0df77ae44
ZJL_OIJR <34391719+ZJLOIJR@users.noreply.github.com>
2018-06-04 10:57:48 +0000

    Update JZOJ4322.cpp

--- JZOJ/JZOJ4322.cpp
+++ JZOJ/JZOJ4322.cpp
@@ -2,30 +2,31 @@
 #include <cstdio>
 #include <cstring>
 #include <cstdlib>
-inline int max(int a, int b) { return a > b ? a : b; }
 
+typedef long long ll;
 const int N = 1e5 + 7;
+inline ll max(ll a, ll b) { return a > b ? a : b; }
 
-int n, len, num[64];
-int tot = 1, son[N * 64][2];
-long long a[N], ans = 0;
+int n, len, num[70];
+int tot = 1, son[N * 70][2];
+ll a[N], ans = 0;
 
 void insert()
 {
 	int now = 1;
-	for (int i = len; i >= 1; i--)
+	for (int i = 64; i >= 1; i--)
 	{
 		if (!son[now][num[i]]) son[now][num[i]] = ++tot;
 		now = son[now][num[i]];
 	}
 }
 
-long long getans()
+ll getans()
 {
-	int now = 1; long long ret = 0;
-	for (int i = len; i >= 1; i--)
+	int now = 1; ll ret = 0;
+	for (int i = 64; i >= 1; i--)
 	{
-		if (son[now][!num[i]]) now = son[now][!num[i]];
+		if (son[now][!num[i]]) now = son[now][!num[i]], ret += (1LL << (i - 1));
 		else now = son[now][num[i]];
 	}
 	return ret;
@@ -33,24 +34,24 @@ long long getans()
 
 int main()
 {
-	//freopen("xor.in", "r", stdin);
-	//freopen("xor.out", "w", stdout);
+	freopen("xor.in", "r", stdin);
+	freopen("xor.out", "w", stdout);
 	
 	scanf("%d", &n);
 	for (int i = 1; i <= n; i++)
 	{
 		scanf("%lld", a + i);
-		len = 0;
-		long long tmp = a[i];
+		len = 0; memset(num, 0, sizeof(num));
+		ll tmp = a[i];
 		while (tmp) num[++len] = tmp & 1, tmp >>= 1;
 		insert();
 	}
 	for (int i = 1; i <= n; i++)
 	{
-		len = 0;
-		long long tmp = a[i];
+		len = 0; memset(num, 0, sizeof(num));
+		ll tmp = a[i];
 		while (tmp) num[++len] = tmp & 1, tmp >>= 1;
-		ans = max(ans, a[i] ^ getans());
+		ans = max(ans, getans());
 	}
 	printf("%lld\n", ans);
 
