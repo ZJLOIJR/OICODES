c1a1f8df5eed6a63e92a0758fc82ee7e00ea7ef9
ZJL_OIJR <34391719+ZJLOIJR@users.noreply.github.com>
2018-06-06 09:41:15 +0000

    Update JZOJ1444.cpp

--- JZOJ/JZOJ1444.cpp
+++ JZOJ/JZOJ1444.cpp
@@ -1,16 +1,16 @@
-#include <map>
 #include <cstdio>
 #include <cstring>
 #include <cstdlib>
-using namespace std;
 
 typedef long long ll;
 const int N = 13;
+const ll P = 10000007;
 
-int n, m, h, t, p, q, ans;
-ll st = 0LL, en, que1[1000010], que2[1000010], pow[N];
+int n, m, h, t, p, q;
+ll st = 0LL, en, que1[1000100], que2[1000100], pow[N];
 int a[N * N], b[N * N];
-map<ll, int> dis1, dis2;
+ll used1[P], used2[P];
+int ans, dis1[P], dis2[P];
 
 inline int min(int a, int b) { return a < b ? a : b; }
 ll swaparr(ll S, int x, int y)
@@ -20,9 +20,16 @@ ll swaparr(ll S, int x, int y)
 	return S - numx * pow[x] - numy * pow[y] + numx * pow[y] + numy * pow[x];
 }
 
+ll find(ll arr[], ll x)
+{
+	ll i = x % P;
+	while (arr[i] && arr[i] != x) i = (i == P) ? 0 : i + 1;
+	return i;
+}
+
 int main()
 {
-	scanf("%d%d", &n, &m); ans = 666666666;
+	scanf("%d%d", &n, &m); ans = n * n;
 	pow[0] = 1; for (int i = 1; i <= n; i++) pow[i] = pow[i - 1] * n;
 	for (int i = 0; i < n; i++) en = en * n + i;
 	for (int i = 1, x; i <= n; i++) scanf("%d", &x), st = st * n + (x - 1);
@@ -32,19 +39,24 @@ int main()
 	while (h <= t && p <= q)
 	{
 		ll S = que1[h++], T = que2[p++];
-		if (dis1[T] < ans)
+		ll tmpS = find(used1, S), tmpT = find(used2, T);
+		if (dis1[tmpS] < ans)
 			for (int i = 1; i <= m; i++)
 			{
 				ll nx = swaparr(S, a[i], b[i]);
-				if (!dis1[nx] && nx != st) dis1[nx] = dis1[S] + 1, que1[++t] = nx;
-				if (dis2[nx] || nx == en) ans = min(ans, dis1[nx] + dis2[nx]);
+				ll tmpnx = find(used1, nx), tpnx = find(used2, nx);
+				used1[tmpnx] = used2[tpnx] = nx;
+				if (!dis1[tmpnx] && nx != st) dis1[tmpnx] = dis1[tmpS] + 1, que1[++t] = nx;
+				if (dis2[tpnx] || nx == en) ans = min(ans, dis1[tmpnx] + dis2[tpnx]);
 			}
-		if (dis2[T] < ans)
+		if (dis2[tmpT] < ans)
 			for (int i = 1; i <= m; i++)
 			{
 				ll nx = swaparr(T, a[i], b[i]);
-				if (!dis2[nx] && nx != en) dis2[nx] = dis2[T] + 1, que2[++q] = nx;
-				if (dis1[nx] || nx == st) ans = min(ans, dis1[nx] + dis2[nx]);
+				ll tmpnx = find(used2, nx), tpnx = find(used1, nx);
+				used2[tmpnx] = used1[tpnx] = nx;
+				if (!dis2[tmpnx] && nx != en) dis2[tmpnx] = dis2[tmpT] + 1, que2[++q] = nx;
+				if (dis1[tpnx] || nx == st) ans = min(ans, dis1[tpnx] + dis2[tmpnx]);
 			}
 	}
 	printf("%d\n", ans);
