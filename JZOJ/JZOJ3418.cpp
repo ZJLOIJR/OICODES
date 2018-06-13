461d097c625d8511104df2d0ff62829bbc01abb8
ZJL_OIJR <34391719+ZJLOIJR@users.noreply.github.com>
2018-06-13 12:09:45 +0000

    Update JZOJ3418.cpp

--- JZOJ/JZOJ3418.cpp
+++ JZOJ/JZOJ3418.cpp
@@ -1,74 +1,55 @@
-#include <cmath>
 #include <cstdio>
 #include <cstring>
 #include <cstdlib>
-#define max(a, b) ((a) > (b) ? (a) : (b))
+#include <algorithm>
+using namespace std;
 
 const int N = 1e3 + 7;
 
-int n, m, fa, cnt = 0, tot = 0, ans = 0, mxi, mxj;
-int st[N], to[N << 1], nx[N << 1], dfn[N], siz[N], w[N];
+int cnt = 0, n, m, v[N], arr[N], dfn[N], out[N];
 int f[N][N], pre[N][N][2];
-
+int tot = 0, st[N], to[N], nx[N];
 inline void add(int u, int v) { to[++tot] = v, nx[tot] = st[u], st[u] = tot; }
-void dfs(int u, int from)
-{
-	dfn[++cnt] = u, siz[u] = 1;
-	for (int i = st[u]; i; i = nx[i])
-		if (to[i] != from) dfs(to[i], u), siz[u] += siz[to[i]];
-}
 
-void print(int i, int j)
+void dfs(int x)
 {
-	if (i == 1 || i == 0) return;
-	print(pre[i][j][0], pre[i][j][1]);
-	if (i != cnt + 1) printf("%d\n", i);
+	dfn[++cnt] = x;
+	for (int i = st[x]; i; i = nx[i]) dfs(to[i]);
+	out[x] = cnt;
 }
 
 int main()
 {
 	scanf("%d%d", &n, &m);
-	for (int i = 1; i <= n; i++)
+	for (int i = 1, fa; i <= n; i++)
 	{
-		scanf("%d%d", &fa, w + i + 1);
-		if (i != fa)
-		{
-			if (fa) add(i + 1, fa + 1), add(fa + 1, i + 1);
-			else add(1, i + 1), add(i + 1, 1);
-		}
+		scanf("%d%d", &fa, v + i);
+		if (fa) add(fa, i); else arr[++arr[0]] = i;
 	}
-	dfs(1, 0);
+	for (int i = 1; i <= arr[0]; i++) dfs(arr[i]);
 	memset(f, -0x3f, sizeof(f));
 	f[1][0] = 0;
-	for (int i = 1; i <= cnt; i++)
+	for (int i = 1; i <= n; i++)
 	{
 		int x = dfn[i];
 		for (int j = 0; j <= m; j++)
 		{
-			if (f[i][j] < 0) continue;
-			if (x == 1)
-				if (f[i][j] > f[i + 1][j])
-				{
-					f[i + 1][j] = f[i][j];
-					pre[i + 1][j][0] = i, pre[i + 1][j][1] = j;
-				}
-			if (j + 1 <= m)
-				if (f[i][j] + w[x] > f[i + 1][j + 1])
-				{
-					f[i + 1][j + 1] = f[i][j] + w[x];
-					pre[i + 1][j + 1][0] = i, pre[i + 1][j + 1][1] = j;
-				}
-			if (f[i][j] > f[i + siz[x]][j])
-			{
-				f[i + siz[x]][j] = f[i][j];
-				pre[i + siz[x]][j][0] = i, pre[i + siz[x]][j][1] = j;
-			}
-			if (f[i][j] > ans) ans = f[i][j], mxi = i, mxj = j;
+			if (f[i][j] + v[x] > f[i + 1][j + 1]) f[i + 1][j + 1] = f[i][j] + v[x], pre[i + 1][j + 1][0] = i, pre[i + 1][j + 1][1] = j;
+			if (f[i][j] > f[out[x] + 1][j]) f[out[x] + 1][j] = f[i][j], pre[out[x] + 1][j][0] = i, pre[out[x] + 1][j][1] = j;
+			if (f[i][j] + v[x] > f[out[x] + 1][j + 1]) f[out[x] + 1][j + 1] = f[i][j] + v[x], pre[out[x] + 1][j + 1][0] = i, pre[out[x] + 1][j + 1][1] = j;
 		}
 	}
-	for (int i = 0; i <= m; i++)
-		if (f[cnt + 1][i] > ans) ans = f[cnt + 1][i], mxi = cnt + 1, mxj = i;
-	printf("%d\n", ans);
-	print(mxi, mxj);
+	printf("%d\n", f[n + 1][m]);
+	if (m >= 100) return 0;
+	int x = n + 1, y = m;
+	memset(arr, 0, sizeof(arr));
+	while (pre[x][y][0])
+	{
+		int nx = pre[x][y][0], ny = pre[x][y][1];
+		if (y != ny) arr[++arr[0]] = dfn[nx];
+		x = nx, y = ny;
+	}
+	sort(arr + 1, arr + arr[0] + 1);
+	for (int i = 1; i <= arr[0]; i++) printf("%d\n", arr[i]);
 	return 0;
 }
