25649e8157b2ba5a8eaa1d792dfecc1f4e9e4ba1
ZJL_OIJR <34391719+ZJLOIJR@users.noreply.github.com>
2018-06-25 04:54:39 +0000

    Update JZOJ1768.cpp

--- JZOJ/JZOJ1768.cpp
+++ JZOJ/JZOJ1768.cpp
@@ -4,30 +4,50 @@
 
 const int N = 100, M = 10;
 
-int n, m, full, f[2][1 << M][1 << M];
-char map[N + 3][M + 3];
+int n, m, full, f[2][1 << M][1 << M], cnt[1 << M], ok[1 << M], h[N + 3];
+char ch;
 
-inline int bit(int x) { return (1 << (x - 1)); }
+inline int max(int a, int b) { return a > b ? a : b; }
 
 int main()
 {
 	scanf("%d%d", &n, &m);
 	for (int i = 1; i <= n; i++)
 		for (int j = 1; j <= m; j++)
-			scanf(" %c", &map[i][j]);
+		{
+			scanf(" %c", &ch);
+			if (ch == 'H') h[i] += (1 << (j - 1));
+		}
 	full = (1 << m) - 1;
-	for (int i = 0; i <= n; i++)
+	for (int s = 0, tmp; s <= full; s++)
 	{
-		for (int j = 0; j <= full; j++)
-			for (int k = 0; k <= full; k++)
+		tmp = s; while (tmp) cnt[s] += (tmp & 1), tmp >>= 1;
+		ok[s] = (s & (s << 1) || s & (s << 2));
+	}
+	for (int s1 = 0; s1 <= full; s1++)
+		for (int s2 = 0; s2 <= full; s2++)
+		{
+			if (ok[s1] || ok[s2] || s1 & s2 || s1 & h[2] || s2 & h[1]) continue;
+			f[1][s2][0] = cnt[s2], f[0][s1][s2] = cnt[s1] + cnt[s2];
+		}
+	for (int i = 2; i < n; i++)
+		for (int s1 = 0; s1 <= full; s1++)
+		{
+			if (ok[s1] || s1 & h[i]) continue;
+			for (int s2 = 0; s2 <= full; s2++)
 			{
-				for (int l = 0; l <= full; l++)
+				if (ok[s2] || s2 & h[i - 1] || s1 & s2) continue;
+				for (int s = 0; s <= full; s++)
 				{
-					if ((j | k) & l) continue;
-					int flag = 0;
-					for (int i = 1; i <= 
+					if (ok[s] || s & (s1 | s2) || s & h[i + 1]) continue;
+					f[(i + 1) & 1][s][s1] = max(f[(i + 1) & 1][s][s1], f[i & 1][s1][s2] + cnt[s]);
 				}
 			}
-	}
+		}
+	int ans = 0;
+	for (int s1 = 0; s1 <= full; s1++)
+		for (int s2 = 0; s2 <= full; s2++)
+			ans = max(ans, f[n & 1][s1][s2]);
+	printf("%d\n", ans);
 	return 0;
 }
