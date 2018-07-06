412a9d4e222f513caa5cd28e3261b17f937c3617
ZJL_OIJR <34391719+ZJLOIJR@users.noreply.github.com>
2018-07-06 10:53:47 +0800

    Update JZOJ3704.cpp

--- JZOJ/JZOJ3704.cpp
+++ JZOJ/JZOJ3704.cpp
@@ -5,23 +5,12 @@
 typedef long long ll;
 const ll P = 1e6 + 7;
 int T;
-ll n, m, k, p, ans, fac[P];
-
-ll pow(ll a, ll b)
-{
-	ll ret = 1;
-	while (b)
-	{
-		if (b & 1) ret = ret * a % p;
-		a = a * a % p, b >>= 1;
-	}
-	return ret;
-}
+ll n, m, k, p, ans, fac[P], inv[P];
 
 ll C(ll n, ll m)
 {
 	if (n < m) return 0;
-	return fac[n] * pow(fac[m] * fac[n - m] % p, p - 2) % p;
+	return fac[n] * inv[m] % p * inv[n - m] % p;
 }
 ll Lucas(ll n, ll m)
 {
@@ -35,10 +24,12 @@ int main()
 	freopen("luckye.out", "w", stdout);
 
 	scanf("%d", &T);
-	fac[0] = 1;
+	fac[0] = inv[0] = inv[1] = 1;
 	while (T--)
 	{
 		scanf("%lld%lld%lld%lld", &n, &m, &k, &p);
+		for (int i = 2; i <= p; i++) inv[i] = (p - p / i) * inv[p % i] % p;
+		for (int i = 2; i <= p; i++) inv[i] = inv[i] * inv[i - 1] % p;
 		for (int i = 1; i <= p; i++) fac[i] = fac[i - 1] * i % p;
 		ans = 0;
 		for (int i = 0, x; i <= n; i++)
