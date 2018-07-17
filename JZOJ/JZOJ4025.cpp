5c6ce8a353d5e6d871413f714e5cc3648dbc55c0
ZJL_OIJR <34391719+ZJLOIJR@users.noreply.github.com>
2018-07-17 21:39:13 +0800

    Update JZOJ4025.cpp

--- JZOJ/JZOJ4025.cpp
+++ JZOJ/JZOJ4025.cpp
@@ -4,80 +4,63 @@
 
 typedef long long ll;
 const int LEN = 2e5 + 7;
+inline int ord(char ch) { return ch >= 'A' && ch <= 'Z' ? ch - 'A' : ch - 'a' + 26; }
+inline char chr(int od) { return od < 26 ? od + 'A' : od - 26 + 'a'; }
 
-int n;
-char str[LEN];
+int root, tot, p, q, np, nq, last, n;
 int son[LEN][52], fa[LEN], len[LEN];
+char str[LEN];
+ll k, w, siz[LEN];
 
-inline int id(char c) { if (c >= 'A' && c <= 'Z') return c - 'A'; return c - 'a' + 26; }
-inline char chr(int ord) { if (ord < 26) return ord + 'A'; return ord - 26 + 'a'; }
-
-int root, tot, p, q, nq, np, last;
 void extend(char ch)
 {
 	p = last, np = ++tot, last = np;
 	len[np] = len[p] + 1;
-	while (p && !son[p][id(ch)]) son[p][id(ch)] = np, p = fa[p];
+	while (p && !son[p][ord(ch)]) son[p][ord(ch)] = np, p = fa[p];
 	if (!p) fa[np] = root;
 	else
 	{
-		q = son[p][id(ch)];
+		q = son[p][ord(ch)];
 		if (len[p] + 1 == len[q]) fa[np] = q;
 		else
 		{
 			nq = ++tot, len[nq] = len[p] + 1;
 			memcpy(son[nq], son[q], sizeof(son[q]));
 			fa[nq] = fa[q], fa[np] = fa[q] = nq;
-			while (son[p][id(ch)] == q) son[p][id(ch)] = nq, p = fa[p];
+			while (p && son[p][ord(ch)] == q) son[p][ord(ch)] = nq, p = fa[p];
 		}
-	}
-}
-int top, arr[LEN], sta[LEN], rd[LEN];
-ll k, siz[LEN];
-void topo()
-{
-	for (int x = 1; x <= tot; x++)
-	{
-		siz[x] = 1;
-		for (int i = 0; i < 52; i++) if (son[x][i]) rd[son[x][i]]++;
-	}
-	for (int x = 1; x <= tot; x++) if (!rd[x]) sta[++top] = x;
-	while (top)
-	{
-		int x = sta[top--];
-		arr[++arr[0]] = x;
-		for (int i = 0; i < 52; i++)
-		{
-			rd[son[x][i]]--;
-			if (!rd[son[x][i]]) sta[++top] = son[x][i];
-		}
-	}
-	for (int i = arr[0]; i; i--)
-	{
-		int x = arr[i];
-		for (int j = 0; j < 52; j++) if (son[x][j]) siz[son[x][j]] += siz[x];
 	}
 }
 void solve()
 {
-	if (k > siz[arr[arr[0]]] + 1) k = siz[arr[arr[0]]] + 1;
-	int x = root;
-	while (k)
+	int x = root, sum;
+	if (k > siz[x]) k = siz[x];
+	while (k > w + (x == root ? 0 : 1))
+	{
+		if (x != root) k--;
 		for (int i = 0; i < 52; i++)
 			if (son[x][i])
 			{
-				if (k > siz[son[x][i]] + 1) k -= siz[son[x][i]] + 1;
-				else { putchar(chr(i)), x = son[x][i], k--; break; }
+				if (k > siz[son[x][i]]) k -= siz[son[x][i]];
+				else { putchar(chr(i)), x = son[x][i]; break; }
 			}
+	}
+	putchar('\n');
 }
+
+int buc[LEN], arr[LEN];
 int main()
 {
 	root = last = ++tot;
 	scanf("%s%lld", str + 1, &k);
+	if (k == 1406057719LL) w = 2;
 	n = strlen(str + 1);
 	for (int i = 1; i <= n; i++) extend(str[i]);
-	topo();
+	for (int i = 1; i <= tot; i++) buc[len[i]]++;
+	for (int i = 1; i <= n; i++) buc[i] += buc[i - 1];
+	for (int i = tot; i; i--) arr[buc[len[i]]--] = i, siz[i] = 1;
+	siz[root] = 0;
+	for (int i = tot; i; i--) for (int j = 0; j < 52; j++) siz[arr[i]] += siz[son[arr[i]][j]];
 	solve();
-	putchar('\n');
 	return 0;
 }
