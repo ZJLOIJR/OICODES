1ef847bb030741ef380180751bf6b399435bf9d8
ZJL_OIJR <34391719+ZJLOIJR@users.noreply.github.com>
2018-08-06 07:36:21 +0800

    Update JZOJ4826.cpp

--- JZOJ/JZOJ4826.cpp
+++ JZOJ/JZOJ4826.cpp
@@ -2,39 +2,22 @@
 #include <cstring>
 #include <cstdlib>
 
+inline int min(int a, int b) { return a < b ? a : b; }
 const int N = 2e3 + 7, M = 2e4 + 7;
-const double eps = 1e-4;
 
 int n, m, u, v, w, tot = 0, st[N], to[M], nx[M], len[M];
-double l, r, mid, ans, dis[N];
+int dis[N][N];
 
 int top = 0, sta[N], arr[N], rd[N];
 
 inline void add(int u, int v, int w) { to[++tot] = v, nx[tot] = st[u], len[tot] = w, st[u] = tot; }
 
-int check()
-{
-	memset(dis, 127, sizeof(dis));
-	dis[n] = 0;
-	for (int i = arr[0]; i; i--)
-	{
-		int u = arr[i];
-		if (u == n) continue;
-		double res = 23333333.0;
-		for (int j = st[u]; j; j = nx[j])
-			if (dis[to[j]] + len[j] - mid < res)
-				res = dis[to[j]] + len[j] - mid;
-		dis[u] = res;
-	}
-	return dis[1] <= 0;
-}
-
 int main()
 {
-	freopen("input", "r", stdin);
-	//freopen("calabash.in", "r", stdin);
-	//freopen("calabash.out", "w", stdout);
+	freopen("calabash.in", "r", stdin);
+	freopen("calabash.out", "w", stdout);
 	
+	memset(dis, 0x3f, sizeof(dis));
 	scanf("%d%d", &n, &m);
 	for (int i = 1, u, v, w; i <= m; i++) scanf("%d%d%d", &u, &v, &w), add(u, v, w), rd[v]++;
 	for (int i = 1; i <= n; i++) if (!rd[i]) sta[++top] = i;
@@ -49,13 +32,13 @@ int main()
 			if (!rd[v]) sta[++top] = v;
 		}
 	}
-	l = 0.0, r = 1e3;
-	while (r - l > eps)
-	{
-		mid = (l + r) / 2;
-		if (check()) r = mid, ans = mid;
-		else l = mid;
-	}
+	dis[n][1] = 0;
+	for (int i = arr[0]; i; i--)
+		for (int j = 2; j <= n; j++)
+			for (int k = st[arr[i]]; k; k = nx[k])
+				dis[arr[i]][j] = min(dis[arr[i]][j], dis[to[k]][j - 1] + len[k]);
+	double ans = 233333333.0;
+	for (int i = 2; i <= n; i++) if (dis[1][i] * 1.0 / i < ans) ans = dis[1][i] * 1.0 / i;
 	printf("%.3lf\n", ans);
 
 	fclose(stdin);
