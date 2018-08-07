e09eaf794ff77f98e899b48fff704fd795a7bd54
ZJL_OIJR <34391719+ZJLOIJR@users.noreply.github.com>
2018-08-07 22:11:27 +0800

    Update JZOJ4841.cpp

--- JZOJ/JZOJ4841.cpp
+++ JZOJ/JZOJ4841.cpp
@@ -1,4 +1,6 @@
+#pragma GCC optimize(2)
 #include <map>
+#include <vector>
 #include <cstdio>
 #include <cstring>
 #include <cstdlib>
@@ -6,42 +8,64 @@ using namespace std;
 
 const int N = 22;
 
-int n, mid, full, ans, a[N];
-map<int, int> buc;
+int n, mid, ans, a[N];
+int vis[1 << 20];
+struct note { int val, status; };
+vector<note> list[60000];
+int tot = 0;
+map<int, int> id;
+
+inline int bit(int x) { return (1 << (x - 1)); }
+
+void insert(note x)
+{
+	if (!id[x.val]) id[x.val] = ++tot;
+	list[id[x.val]].push_back(x);
+}
+
+void find(note x)
+{
+	if (!id[x.val]) return;
+	int tmpid = id[x.val], siz = list[tmpid].size();
+	for (int i = 0; i < siz; i++)
+		if (!vis[x.status | list[tmpid][i].status])
+		{
+			vis[x.status | list[tmpid][i].status] = 1;
+			ans++;
+		}
+}
+
+void dfs1(int now, int sum, int s)
+{
+	if (now > mid) { insert((note){sum, s}); return; }
+	dfs1(now + 1, sum + a[now], s | bit(now));
+	dfs1(now + 1, sum - a[now], s | bit(now));
+	dfs1(now + 1, sum, s);
+}
+
+void dfs2(int now, int sum, int s)
+{
+	if (now > n)
+	{
+		find((note){sum, s});
+		return;
+	}
+	dfs2(now + 1, sum + a[now], s | bit(now));
+	dfs2(now + 1, sum - a[now], s | bit(now));
+	dfs2(now + 1, sum, s);
+}
 
 int main()
 {
-	//freopen("subset.in", "r", stdin);
-	//freopen("subset.out", "w", stdout);
+	freopen("subset.in", "r", stdin);
+	freopen("subset.out", "w", stdout);
 
 	scanf("%d", &n);
 	for (int i = 1; i <= n; i++) scanf("%d", a + i);
 	mid = (1 + n) / 2;
-	full = 1; for (int i = 1; i <= mid; i++) full *= 3; full--;
-	for (int s = 0; s <= full; s++)
-	{
-		int tmp = s, ret = 0, now = 1;
-		while (tmp)
-		{
-			if (tmp % 3 == 1) ret += a[now];
-			else if (tmp % 3 == 2) ret -= a[now];
-			tmp /= 3, now++;
-		}
-		buc[ret]++;
-	}
-	full = 1; for (int i = mid + 1; i <= n; i++) full *= 3; full--;
-	for (int s = 0; s <= full; s++)
-	{
-		int tmp = s, ret = 0, now = mid + 1;
-		while (tmp)
-		{
-			if (tmp % 3 == 1) ret += a[now];
-			else if (tmp % 3 == 2) ret -= a[now];
-			tmp /= 3, now++;
-		}
-		ans += buc[ret];
-	}
-	printf("%d\n", (ans - 1) / 2);
+	dfs1(1, 0, 0);
+	dfs2(mid + 1, 0, 0);
+	printf("%d\n", ans - 1);
 
 	fclose(stdin);
 	fclose(stdout);
