e7007fd5189e0b12e7d133c61239050585af3a04
ZJL_OIJR <34391719+ZJLOIJR@users.noreply.github.com>
2018-09-19 12:17:30 +0000

    Update JZOJ1725.cpp

--- JZOJ/JZOJ1725.cpp
+++ JZOJ/JZOJ1725.cpp
@@ -1,19 +1,36 @@
+#include <cmath>
 #include <cstdio>
 #include <cstring>
 #include <cstdlib>
-#include <algorithm>
-using namespace std;
 
 const int N = 57;
 
 int T;
-int n, x[N], y[N], r[N];
+int n, x[N], y[N], r[N], X1, Y1, X2, Y2, u, v, ur = 2333, vr = 2333;
+int t[N];
+
+double sqr(double a) { return a * a; }
+double getdis(double X1, double Y1, double X2, double Y2)
+{
+	return sqrt(sqr(X1 - X2) + sqr(Y1 - Y2));
+}
 
 int main()
 {
 	scanf("%d", &T);
 	while (T--)
 	{
+		scanf("%d", &n);
+		for (int i = 1; i <= n; i++) scanf("%d", x + i);
+		for (int i = 1; i <= n; i++) scanf("%d", y + i);
+		for (int i = 1; i <= n; i++) scanf("%d", r + i);
+		for (int i = 1; i <= n; i++)
+		{
+			if (getdis(x[i], y[i], X1, Y1) <= r[i] && r[i] < ur) u = i, ur = r[i];
+			if (getdis(x[i], y[i], X2, Y2) <= r[i] && r[i] < vr) v = i, vr = r[i];
+		}
+		for (int i = 1; i <= n; i++)
+			if (getdis(x[i], y[i], x[u], y[u]) + r[u] <= r[i]) t[i] = 1;
 	}
 	return 0;
 }
