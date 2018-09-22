37da2108b8bb4d2bbe817e2cde5aa29e9a51d424
ZJL_OIJR <34391719+ZJLOIJR@users.noreply.github.com>
2018-09-22 16:39:10 +0800

    Update JZOJ5343.cpp

--- JZOJ/JZOJ5343.cpp
+++ JZOJ/JZOJ5343.cpp
@@ -5,8 +5,8 @@
 typedef long long ll;
 const int N = 2e6 + 7;
 
-int n, s[N], c[N], a[N];
-ll sum, ans = 233333333;
+int n, s[N];
+ll sum, ans = 233333333, c[N], a[N];
 
 void rplus(int l, int r, int val) { c[l] += val, c[r + 1] -= val; }
 
@@ -21,20 +21,32 @@ int main()
 	for (int i = 1; i <= n; i++)
 	{
 		scanf("%d", s + i);
+		if (s[i] > i)
+		{
+			add(0, i - 1, s[i] - i, 1);
+			add(i, i + n - s[i], n - s[i], -1);
+			add(i + n - s[i], n, 0, 1);
+		}
 		if (s[i] < i)
 		{
-			add(i, n - 1, i - s[i], 1);
+			add(i, n, n - s[i], -1);
+			add(0, i - s[i], i - s[i], -1);
 			add(i - s[i], i - 1, 0, 1);
-			add();
+		}
+		if (s[i] == i)
+		{
+			add(0, i - 1, 0, 1);
+			add(i, n, n - s[i], -1);
 		}
 	}
 	sum = 0;
-	for (int i = 1; i <= n; i++) sum += c[i], a[i] = sum;
+	for (int i = 0; i <= n; i++) sum += c[i], a[i] = sum;
 	sum = 0;
-	for (int i = 1; i <= n; i++)
+	for (int i = 0; i <= n; i++)
 	{
 		sum += a[i];
 		if (sum < ans) ans = sum;
+		printf("%d ", sum);
 	}
 	printf("%lld\n", ans);
 	return 0;
